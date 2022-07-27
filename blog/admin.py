e4334860ac65034c9d02f348846b8d19722a4d8e
Mohammad <mohammad@ubuntu.ubuntu-domain>
2022-07-27 19:12:44 +0300

    Create by  class Post in blog and AdminBlog and about class Contact, AdminContact in website app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'published_date', 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+# admin.site.register(Post)
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from website.models import Contact
 
 # Register your models here.
+class AdminContact(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'email', 'created_date',)
+    list_filter = ('email',)
+    search_fields = ('name', 'message')
+admin.site.register(Contact, AdminContact)

36009c159899abf59e2b1e1da65332cfb9a6bbab
mfarjami <biroghlan95@gmail.com>
2022-08-05 21:32:06 +0300

    Create postcategories to templatetags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -8,4 +8,13 @@ def latestposts():
     posts = Post.objects.filter(status=True).order_by('-published_date')[:3]
     return {'posts':posts}
 
+
+@register.inclusion_tag('blog/blog-category.html')
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories':cat_dict}
 

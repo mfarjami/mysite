dd855134fbc09cad336cf8037c53197667149bdc
mfarjami <biroghlan95@gmail.com>
2022-08-07 20:28:52 +0300

    Filter by author

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,18 @@
+from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=True)
+    if kwargs.get('cat_name') != None:
+        posts = Post.objects.filter(status=True, category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     return render(request, 'blog/blog-home.html', {'posts': posts})
 
 
 def blog_single(request, pk):
     post = get_object_or_404(Post, pk=pk, status=1)
     return render(request, 'blog/blog-single.html', {'post': post})
-
-def blog_category(request, cat_name):
-    posts = Post.objects.filter(status=True, category__name=cat_name)
-    return render(request, 'blog/blog-home.html', {'posts': posts})

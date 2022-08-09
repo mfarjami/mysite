7c2d0f6c2414e1709967c1077d35721e2fe31db0
mfarjami <biroghlan95@gmail.com>
2022-08-09 12:43:21 +0300

    Set up pagination

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,20 @@
+from pydoc import pager
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 
 
 def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=True)
+    
     if kwargs.get('cat_name') != None:
         posts = Post.objects.filter(status=True, category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts, 1)
+    page_number = request.GET.get('page')
+    posts = posts.get_page(page_number)
     return render(request, 'blog/blog-home.html', {'posts': posts})
 
 
@@ -19,7 +25,7 @@ def blog_single(request, pk):
 def blog_search(request):
     posts=Post.objects.filter(status=True)
     if request.method == 'GET':
-        if s := request.Get.get('q'):
+        if s := request.GET.get('q'):
             posts = posts.filter(content__contains=s)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
